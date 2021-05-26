eec717dd8783aee7fc17e0b73897f0d9419e11dd
AAellen <aaellen01@gmail.com>
2021-05-26 22:12:44 +0100

    Minor changes to logo slide

--- assets/js/logo_slide.js
+++ assets/js/logo_slide.js
@@ -2,53 +2,63 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // find all places to put a logo slide
     var logos =document.getElementsByClassName("gen-logo-slide");
     for(var i=0; i<logos.length; i++){
-        
-        // create all the divs for each part of the logo slide
-        var parent = logos[i].parentElement;
-        var container = document.createElement(logos[i].id);
-        var before = document.createElement("div");
-        var after = document.createElement("div");
-        var text = document.createElement("div");
-        container.appendChild(before);
-        container.appendChild(text);
-        container.appendChild(after);
-        parent.insertBefore(container, logos[i]);
+  
+      // create all the divs for each part of the logo slide
+      var parent = logos[i].parentElement;
+      var container = document.createElement(logos[i].id);
+      var before = document.createElement("div");
+      var after = document.createElement("div");
+      var text = document.createElement("div");
+      container.appendChild(before);
+      container.appendChild(text);
+      container.appendChild(after);
+      parent.insertBefore(container, logos[i]);
   
   
-        container.className = "logo-slide";
-        before.innerText = "</";
-        text.innerText = logos[i].innerText;
-        after.innerText = ">";
-        before.className="logo-slide-before noselect";
-        text.className="logo-slide-text noselect";
-        after.className="logo-slide-after noselect";
-      
-        text.style.display="inline-block";
-        outside(null);
-        
-        // slide in text on mouse hover
-        container.addEventListener("mouseover", function( event ){    
-            text.style.transform="translateX(0)";
-            text.style.opacity="1";
-    
-            before.style.paddingLeft="0";
-            after.style.transform = "";
-            
-        }, false);
+      container.className = "logo-slide";
+      before.innerText = "</";
+      text.innerText = logos[i].innerText;
+      after.innerText = ">";
+      before.className="logo-slide-before noselect";
+      text.className="logo-slide-text noselect";
+      after.className="logo-slide-after noselect";
   
-        container.addEventListener("mouseout", outside);
-
-        // slide out text when mouse leaves and when page is first loaded    
-        function outside(event){
-            var W = text.offsetWidth
-            text.style.transform=""; // reset back to default defined in .css file
-            text.style.opacity = "0";
-            before.style.paddingLeft=`${W}px`;
-            after.style.transform=`translateX(-${W}px)`;
-    
-        }
+      text.style.display="inline-block";
+      outside();
+      var insideTimer;
+      var outsideTimer;
   
-        logos[i].remove();
+      // slide in text on mouse hover
+      container.addEventListener("mouseenter", function( event ){ 
+        clearTimeout(outsideTimer);
+  
+        insideTimer= setTimeout(()=> {
+          text.style.transform="translateX(0)";
+          text.style.opacity="1";
+  
+          before.style.paddingLeft="0";
+          after.style.transform = "";
+        }, 100);
+  
+      }, false);
+  
+      container.addEventListener("mouseleave", outside);
+  
+      // slide out text when mouse leaves and when page is first loaded    
+      function outside(){
+        clearTimeout(insideTimer);
+  
+        outsideTimer = setTimeout(() => {
+          var W = text.offsetWidth
+          text.style.transform=""; // reset back to default defined in .css file
+          text.style.opacity = "0";
+          before.style.paddingLeft=`${W-before.offsetWidth}px`;
+          after.style.transform=`translateX(-${W}px)`;
+  
+        }, 100)
+      }
+  
+      logos[i].remove();
     }
   
-});
+  });
