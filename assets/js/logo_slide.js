befc46a0fe830ff972d3191adbc4fe26796717d9
AAellen <aaellen01@gmail.com>
2021-05-26 21:14:58 +0100

    Minor changes
    Add comments
    Remove console.logs

--- assets/js/logo_slide.js
+++ assets/js/logo_slide.js
@@ -1,56 +1,56 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    // find all places to put a logo slide
     var logos =document.getElementsByClassName("gen-logo-slide");
     for(var i=0; i<logos.length; i++){
-  
-      var parent = logos[i].parentElement;
-      var container = document.createElement(logos[i].id);
-      var before = document.createElement("div");
-      var after = document.createElement("div");
-      var text = document.createElement("div");
-      container.appendChild(before);
-      container.appendChild(text);
-      container.appendChild(after);
-      parent.insertBefore(container, logos[i]);
-  
-  
-      container.className = "logo-slide";
-      before.innerText = "</";
-      text.innerText = logos[i].innerText;
-      after.innerText = ">";
-      before.className="logo-slide-before noselect";
-      text.className="logo-slide-text noselect";
-      after.className="logo-slide-after noselect";
-      
-      text.style.display="inline-block";
-      outside(null);
-  
-      container.addEventListener("mouseover", function( event ){
-        var fullW = text.offsetWidth;
-  
-        text.style.transform="translateX(0)";
-        text.style.opacity="1";
-  
-        before.style.paddingLeft="0";
-        after.style.transform = "";
         
-        console.log("inside")
+        // create all the divs for each part of the logo slide
+        var parent = logos[i].parentElement;
+        var container = document.createElement(logos[i].id);
+        var before = document.createElement("div");
+        var after = document.createElement("div");
+        var text = document.createElement("div");
+        container.appendChild(before);
+        container.appendChild(text);
+        container.appendChild(after);
+        parent.insertBefore(container, logos[i]);
   
-      }, false);
   
-      container.addEventListener("mouseout", outside);
+        container.className = "logo-slide";
+        before.innerText = "</";
+        text.innerText = logos[i].innerText;
+        after.innerText = ">";
+        before.className="logo-slide-before noselect";
+        text.className="logo-slide-text noselect";
+        after.className="logo-slide-after noselect";
+      
+        text.style.display="inline-block";
+        outside(null);
+        
+        // slide in text on mouse hover
+        container.addEventListener("mouseover", function( event ){
+            var fullW = text.offsetWidth;
+    
+            text.style.transform="translateX(0)";
+            text.style.opacity="1";
+    
+            before.style.paddingLeft="0";
+            after.style.transform = "";
+            
+        }, false);
   
-      function outside(event){
-      var W = text.offsetWidth
-        text.style.transform=""; // reset back to default defined in .css
-        text.style.opacity = "0";
-        before.style.paddingLeft=`${W}px`;
-        after.style.transform=`translateX(-${W}px)`;
-        console.log("outside")
+        container.addEventListener("mouseout", outside);
+
+        // slide out text when mouse leaves and when page is first loaded    
+        function outside(event){
+        var W = text.offsetWidth
+            text.style.transform=""; // reset back to default defined in .css file
+            text.style.opacity = "0";
+            before.style.paddingLeft=`${W}px`;
+            after.style.transform=`translateX(-${W}px)`;
+    
+        }
   
-      }
-  
-          console.log("removiing")
-      logos[i].remove();
+        logos[i].remove();
     }
   
-  });
+});
